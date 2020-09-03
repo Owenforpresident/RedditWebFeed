cfb23706997d65331566f3eeef8bba1cf72c5fef
EC2 Default User <ec2-user@ip-172-31-26-85.eu-west-1.compute.internal>
2020-09-03 12:59:21 +0000

    update gitignore

---
+++ src/aws-exports.js
@@ -0,0 +1,17 @@
+const awsmobile = {
+    "aws_project_region": "eu-west-1",
+    "aws_cognito_identity_pool_id": "eu-west-1:192e4743-099e-4dac-82b1-5ff90c0e5c66",
+    "aws_cognito_region": "eu-west-1",
+    "aws_user_pools_id": "eu-west-1_4zIfzIk8d",
+    "aws_user_pools_web_client_id": "26ammgu0hpcn9q9ksaonehi55r",
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://xfokpagws5glrpmxui2snvh3y4.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_content_delivery_bucket": "reddit-20200812093205-hostingbucket-dev",
+    "aws_content_delivery_bucket_region": "eu-west-1",
+    "aws_content_delivery_url": "http://reddit-20200812093205-hostingbucket-dev.s3-website-eu-west-1.amazonaws.com"
+};
+
+
+export default awsmobile;
